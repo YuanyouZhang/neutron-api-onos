1f610dc8d32984e89c3c0f1be32b4726181ef490
zhangyuanyou <zhangyuanyou@huawei.com>
2016-07-27 17:15:43 +0800

    add update sfc when install

--- hooks/onos_package.py
+++ hooks/onos_package.py
@@ -13,13 +13,14 @@ ML2_CONF = '%s/plugins/ml2/ml2_conf.ini' % NEUTRON_CONF_DIR
 # Packages to be installed by charm.
 def install_packages(servicename):
     # Install drivers
-    apt_install(['git'], fatal=True)
-    check_call("sudo tar xvf files/networking-onos.tar", shell=True)
-    check_call("cd networking-onos;sudo ./install_driver.sh;cd ..", shell=True)
-    check_call("sudo tar xvf files/networking-sfc.tar", shell=True)
-    check_call("cd networking-sfc;sudo ./install_driver.sh;cd ..", shell=True)
-    # Install neutron
-    pkgs = ['neutron-common', 'neutron-plugin-ml2']
-    pkgs = filter_installed_packages(pkgs)
-    apt_install(pkgs, fatal=True)
-    #check_call("sudo neutron-db-manage --subproject networking-sfc upgrade head", shell=True)
+    if hook_name() == "install":
+        apt_install(['git'], fatal=True)
+        check_call("sudo tar xvf files/networking-onos.tar", shell=True)
+        check_call("cd networking-onos;sudo ./install_driver.sh;cd ..", shell=True)
+        check_call("sudo tar xvf files/networking-sfc.tar", shell=True)
+        check_call("cd networking-sfc;sudo ./install_driver.sh;cd ..", shell=True)
+        # Install neutron
+        pkgs = ['neutron-common', 'neutron-plugin-ml2']
+        pkgs = filter_installed_packages(pkgs)
+        apt_install(pkgs, fatal=True)
+        check_call("sudo neutron-db-manage --subproject networking-sfc upgrade head", shell=True)
